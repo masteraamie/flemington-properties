880cb66f0fce87333dc69b5136a204b5d7e9f926
Aamir Amin <masteraamie@gmail.com>
2025-06-18 00:05:18 +0530

    update

--- includes/logo.php
+++ includes/logo.php
@@ -1,3 +1,5 @@
-<div class="logo-icon">
-    <img src="/assets/images/logo.png" alt="Logo" style="width: 50px; height: 50px; margin-top: 5px;">
-</div>
+<a href="/">
+    <div class="logo-icon">
+        <img src="/assets/images/logo.png" alt="Logo" style="width: 50px; height: 50px; margin-top: 5px;">
+    </div>
+</a>
